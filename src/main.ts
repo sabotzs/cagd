e96dc40588e15c60ac0ea02b19826f86a73a7f73
sabotzs <georgi.kuklev@protonmail.com>
2025-07-02 22:56:05 +0300

    Implement adding points with the mouse and draw the path from them.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
+import { drawPolygon } from "./Draw"
+import { Vec2 } from "./Vec2"
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+
+const points: Vec2[] = []
 
 window.addEventListener("load", () => {
     canvas.width = canvas.clientWidth
     canvas.height = canvas.clientHeight
+})
+
+canvas.addEventListener("mousedown", (event) => {
+    const point = mouseLocationInCanvas(event)
+
+    points.push(point)
     draw()
 })
 
 function draw() {
-    ctx.beginPath()
-    ctx.moveTo(10, 10)
-    ctx.lineTo(40, 30)
-    ctx.lineTo(10, 50)
-    ctx.closePath()
-    ctx.stroke()
+    ctx.reset()
+    drawPolygon(ctx, points)
+}
+
+function mouseLocationInCanvas(event: MouseEvent): Vec2 {
+    const rect = canvas.getBoundingClientRect()
+    return Vec2(event.clientX - rect.x, event.clientY - rect.y)
 }
