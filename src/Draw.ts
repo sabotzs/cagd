7dcb95290d77af3c274e7aefd5cba495fb76bee9
sabotzs <georgi.kuklev@protonmail.com>
2025-07-02 22:40:27 +0300

    Add functions for drawing.

---
+++ src/Draw.ts
@@ -0,0 +1,45 @@
+import { Vec2 } from "./Vec2"
+
+export function drawPath(
+    ctx: CanvasRenderingContext2D,
+    points: Vec2[],
+    pointStyle: string = "rgb(128, 0, 0)",
+    lineStyle: string = "rgb(0, 0, 0)"
+) {
+    setStyle(ctx, pointStyle)
+    points.forEach((point) => drawPoint(ctx, point))
+
+    if (points.length < 2) {
+        return
+    }
+
+    setStyle(ctx, lineStyle)
+    ctx.beginPath()
+    ctx.moveTo(points[0].x, points[0].y)
+    for (let i = 1; i < points.length; ++i) {
+        ctx.lineTo(points[i].x, points[i].y)
+    }
+    ctx.stroke()
+}
+
+export function drawLine(ctx: CanvasRenderingContext2D, from: Vec2, to: Vec2) {
+    ctx.beginPath()
+    ctx.moveTo(from.x, from.y)
+    ctx.lineTo(to.x, to.y)
+    ctx.stroke()
+}
+
+function drawPoint(
+    ctx: CanvasRenderingContext2D,
+    point: Vec2,
+    radius: number = 4
+) {
+    ctx.beginPath()
+    ctx.arc(point.x, point.y, radius, 0, 2 * Math.PI)
+    ctx.fill()
+}
+
+function setStyle(ctx: CanvasRenderingContext2D, style: string) {
+    ctx.fillStyle = style
+    ctx.strokeStyle = style
+}
