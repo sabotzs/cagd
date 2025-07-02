7d04e011422a69b6e23733a99fc8b6f657baecba
sabotzs <georgi.kuklev@protonmail.com>
2025-07-03 00:04:49 +0300

    Implement moving of points.

--- src/main.ts
+++ src/main.ts
@@ -6,31 +6,51 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
 const mouseHandleRadius = 8
 const points: Vec2[] = []
+let movedPoint: Vec2 | undefined
 
 window.addEventListener("load", () => {
     canvas.width = canvas.clientWidth
     canvas.height = canvas.clientHeight
 })
 
+canvas.addEventListener("mousedown", handleMouseDown)
+canvas.addEventListener("mousemove", handleMouseMove)
+canvas.addEventListener("mouseup", handleMouseUp)
+
+// suppress contex menu for removing points on right click
 canvas.addEventListener("contextmenu", (event) => event.preventDefault())
 
-canvas.addEventListener("mousedown", (event) => {
+function handleMouseDown(event: MouseEvent) {
     const mousePoint = mouseLocationInCanvas(event)
+    const selectedPointIndex = points.findIndex((point) => {
+        const dx = point.x - mousePoint.x
+        const dy = point.y - mousePoint.y
+        return dx * dx + dy * dy <= mouseHandleRadius * mouseHandleRadius
+    })
 
-    if (event.button === 0) {
-        points.push(mousePoint)
-    } else if (event.button === 2) {
-        const selectedPointIndex = points.findIndex((point) => {
-            const dx = point.x - mousePoint.x
-            const dy = point.y - mousePoint.y
-            return dx * dx + dy * dy <= mouseHandleRadius * mouseHandleRadius
-        })
-        if (selectedPointIndex !== -1) {
+    if (selectedPointIndex !== -1) {
+        if (event.button === 0) {
+            movedPoint = points[selectedPointIndex]
+        } else if (event.button === 2) {
             points.splice(selectedPointIndex, 1)
         }
+    } else if (event.button === 0) {
+        points.push(mousePoint)
     }
     draw()
-})
+}
+
+function handleMouseMove(event: MouseEvent) {
+    if (movedPoint) {
+        movedPoint.x += event.movementX
+        movedPoint.y += event.movementY
+        draw()
+    }
+}
+
+function handleMouseUp() {
+    movedPoint = undefined
+}
 
 function draw() {
     ctx.reset()
